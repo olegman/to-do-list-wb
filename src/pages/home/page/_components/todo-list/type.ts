8cf52eb9d6f511185794c39fd55f565f7681331d
Oleg Martynov <olegman.ru@gmail.com>
2023-09-22 15:14:40 +0300

    UI (#1)
    * feat: ui
    
    * fix: paddings and margins must be divisible by 4

---
+++ src/pages/home/page/_components/todo-list/type.ts
@@ -0,0 +1,7 @@
+export type TTodo = {
+  id: number;
+  name: string;
+  description: string;
+  date: string;
+  check: boolean;
+};
--- src/pages/home/page/index.tsx
+++ src/pages/home/page/index.tsx
@@ -1,8 +1,8 @@
 import React, { memo } from 'react';
 import classnames from 'classnames/bind';
 import styles from './index.module.scss';
-import { Card } from './_components/card';
 import { Header } from './_components/header';
+import { TodoList } from './_components/todo-list';
 
 const cn = classnames.bind(styles);
 
@@ -11,8 +11,8 @@ const BLOCK_NAME = 'Home-page';
 export const Page = memo(() => (
   <div className={cn(BLOCK_NAME)} data-page="home-page">
     <Header />
-    <div className={cn(`${BLOCK_NAME}__card-wrapper`)}>
-      <Card />
+    <div className={cn(`${BLOCK_NAME}__content-wrapper`)}>
+      <TodoList />
     </div>
   </div>
 ));
