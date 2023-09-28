6c3be40c5aaf15188527633171e143e4c7b7fda9
olegman <olegman.ru@gmail.com>
2023-09-28 10:35:45 +0300

    feat: i18n

---
+++ src/_constants/i18next/translations.ts
@@ -0,0 +1,8 @@
+import { APP_NAMESPACE } from './app-namespace';
+
+export const TRANSLATIONS = {
+  addTodoButton: `${APP_NAMESPACE}:addTodoButton`,
+  editTodoButton: `${APP_NAMESPACE}:editTodoButton`,
+  removeTodoButton: `${APP_NAMESPACE}:removeTodoButton`,
+  saveTodoButton: `${APP_NAMESPACE}:saveTodoButton`,
+};
--- src/pages/home/route.ts
+++ src/pages/home/route.ts
@@ -1,9 +1,13 @@
+import { APP_NAMESPACE } from '@/_constants/i18next/app-namespace';
+import { I18N_DICTIONARY } from '@/_assets/i18next/dictionary';
+
 export default {
   name: 'home',
   path: '/home',
   loadAction: () => import('./index'),
   // uncomment if you need translations
-  // i18n: {
-  //   namespaces: [appNamespace],
-  // },
+  i18n: {
+    namespaces: [APP_NAMESPACE],
+    localDictionaryFiles: I18N_DICTIONARY,
+  },
 };
